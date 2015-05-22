c7cb4670d135462029633982248a653ba4d7ac9e
arasyi <arasyi@users.noreply.github.com>
2015-05-22 07:57:04 +0700

    Initial commit

---
+++ kissanime-dta.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name           KissAnime DTA
+// @namespace      http://arasyi/kissanime-dta
+// @author         Arif Rasyidi <arasyi@gmail.com>
+// @version        1.0
+// @description    Display download links for all episodes on KissAnime so we can use DownThemAll! or similar tools to download them.
+// @match          *://kissanime.com/Anime/*
+// @grant          none
+// @updateURL      http://arasyi.github.io/KissAnime-DTA/kissanime-dta.meta.js
+// @downloadURL    http://arasyi.github.io/KissAnime-DTA/kissanime-dta.user.js
+// ==/UserScript==
